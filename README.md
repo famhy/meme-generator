76ca088621b6bbfa400d79179c31ea6de905f5a1
Fehmi Ben zaghden <fahmidouiri@gmail.com>
2022-03-25 23:07:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# React Meme Generator
+
+> A Web App to generate and download custom Memes built with React
+
+## Dependencies
+
+- [React](https://reactjs.org/)
+
+## Deployment
+
+Create a [Netlify](https://www.netlify.com/) Account and then follow the instructions there to deploy from GitHub.
+
+## Preview
+
+![Preview of the app](/images/page.png "Preview of the app")
---
+++ src/components/InputFields.js
@@ -0,0 +1,130 @@
+import { useEffect, useState } from "react";
+
+// Helper function for escaping not allowed Character in the URL for the api call
+function escapingCharacters(item) {
+  if (item === " ") {
+    return "_";
+  } else if (item === "_") {
+    return "__";
+  } else if (item === "-") {
+    return "--";
+  } else if (item === "?") {
+    return "~q";
+  } else if (item === "&") {
+    return "~a";
+  } else if (item === "%") {
+    return "~p";
+  } else if (item === "#") {
+    return "~h";
+  } else if (item === "/") {
+    return "~s";
+  } else if (item === "\\") {
+    return "~b";
+  } else if (item === '"') {
+    return "''";
+  } else {
+    return item;
+  }
+}
+
+// Helper function to download the created meme
+function download(downloadUrl, fileText) {
+  fetch(downloadUrl.split(".").slice(0, 3).join(".") + ".png", {
+    method: "GET",
+    headers: {},
+  })
+    .then((response) => {
+      response.arrayBuffer().then(function (buffer) {
+        const url = window.URL.createObjectURL(new Blob([buffer]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", fileText + ".png");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export default function InputFields(props) {
+  // Save the top text
+  const [topText, setTopText] = useState("");
+  // Save the bottom text
+  const [bottomText, setBottomText] = useState("");
+  return (
+    <div className="inputFields">
+      <div>
+        <label htmlFor="topText">Top Text</label>
+        <input
+          id="topText"
+          type="text"
+          onChange={({ target }) => {
+            const urlText = target.value
+              .split("")
+              .map((item) => escapingCharacters(item));
+            props.setTopText(urlText.join(""));
+            setTopText(urlText.join(""));
+
+            console.log(
+              `api.memegen.link/images/${props.chosenMeme}/${urlText.join(
+                ""
+              )}/${bottomText}.jpg?width=450&height=450`
+            );
+            props.setMemeUrl(
+              `https://api.memegen.link/images/${
+                props.chosenMeme
+              }/${urlText.join("")}/${bottomText}.jpg?width=450&height=450`
+            );
+          }}
+        />
+      </div>
+      <div>
+        <label htmlFor="bottomText">Bottom Text</label>
+        <input
+          id="bottomText"
+          type="text"
+          onChange={({ target }) => {
+            const urlText = target.value
+              .split("")
+              .map((item) => escapingCharacters(item));
+            props.setBottomText(urlText.join(""));
+            setBottomText(urlText.join(""));
+            console.log(
+              `api.memegen.link/images/${
+                props.chosenMeme
+              }/${topText}/${urlText.join("")}.jpg?width=450&height=450`
+            );
+            props.setMemeUrl(
+              `https://api.memegen.link/images/${
+                props.chosenMeme
+              }/${topText}/${urlText.join("")}.jpg?width=450&height=450`
+            );
+          }}
+        />
+      </div>
+      <button
+        onClick={() => {
+          props.setOverlayHidden(false);
+        }}
+      >
+        Choose Meme
+      </button>
+      <button
+        onClick={() => {
+          console.log(props.chosenMeme);
+          props.setMemeUrl(
+            `https://api.memegen.link/images/${props.chosenMeme}/${props.topText}/${props.bottomText}.jpg?width=450&height=450`
+          );
+        }}
+      >
+        Create Meme
+      </button>
+      <button onClick={() => download(props.memeUrl, props.chosenMeme)}>
+        Download
+      </button>
+    </div>
+  );
+}
